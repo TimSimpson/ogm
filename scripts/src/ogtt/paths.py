01ae8d19346af6efaeded9993572a49f7f3cfb5b
Tim Simpson <timsimpson4@gmail.com>
2024-10-06 13:00:28 -0500

    more moving stuff around

--- scripts/src/ogtt/paths.py
+++ scripts/src/ogtt/paths.py
@@ -4,8 +4,8 @@ import typing as t
 
 _ROOT_STR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../..")
 ROOT = pathlib.Path(_ROOT_STR)
-DEPS = ROOT / "deps"
+DEPS = ROOT / "../deps"
 GENERATORS = ROOT / "../generators"
-SPECS = DEPS / "specs"
+SPECS = DEPS / "../specs"
 OPEN_API_CLI = DEPS / "openapi-generator"
-OUTPUT = ROOT / "output"
+OUTPUT = ROOT / "../output"
